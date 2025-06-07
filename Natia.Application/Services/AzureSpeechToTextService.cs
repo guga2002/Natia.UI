e65c286db35eaa7bddad9d2259eee73f0fc00098
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-06-07 15:36:38 -0700

    add wav

--- Natia.Application/Services/AzureSpeechToTextService.cs
+++ Natia.Application/Services/AzureSpeechToTextService.cs
@@ -22,16 +22,17 @@ public class AzureSpeechToTextService : IAzureSpeechToTextService
         _httpClient.DefaultRequestHeaders.Clear();
         _httpClient.DefaultRequestHeaders.Add("User-Agent", "MyApp/1.0");
         _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
-        _httpClient.DefaultRequestHeaders.Add("X-Microsoft-OutputFormat", "audio-16khz-32kbitrate-mono-mp3");
+        _httpClient.DefaultRequestHeaders.Add("X-Microsoft-OutputFormat", "riff-16khz-16bit-mono-pcm"); 
 
         var requestBody = $@"
-            <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='{language}'>
-                <voice name='{voiceName}'>{text}</voice>
-            </speak>";
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='{language}'>
+            <voice name='{voiceName}'>{System.Net.WebUtility.HtmlEncode(text)}</voice>
+        </speak>";
 
         var content = new StringContent(requestBody, Encoding.UTF8, "application/ssml+xml");
         var response = await _httpClient.PostAsync(url, content);
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsByteArrayAsync();
     }
+
 }
